c262453250da5d5ed17dc9f1c3d41298586a47da
Rahul Shankar Rathod <137203986+RahulRathod007@users.noreply.github.com>
2026-01-01 17:03:17 +0530

    Refactor LeftRightDiagonalSum to LeftDiagonalSumMatrix
    modified logic

--- Array Programs/Two Diamentional Array/LeftRightDiagonalSum.java
+++ Array Programs/Two Diamentional Array/LeftRightDiagonalSum.java
@@ -1,12 +1,25 @@
-//WAP to create 3x3 matrixes and calculate the sum of left & right diagonal of element.
-import java.util.*;
-public class LeftRightDiagonalSum
-{
-	public static void main(String x[])
-	{
+/*
+1     2    3 
+4     5    6 
+7     8    9 
+
+OUTPUT : 
+lsum = 1 + 5 + 9 = 15 
+rsum = 3 + 5 + 7 = 15
+*/
+package LeftRightDiagonalSum;
+
+import java.util.Scanner;
+
+public class LeftDiagonalSumMatrix {
+
+	public static void main(String[] args) {
+		
 		Scanner xyz = new Scanner(System.in);
-		int a[][]= new int [3][3];
-		System.out.println("\n Enter a 3 x 3 Matrix values \n");
+		
+		System.out.println("Enter values in matrix : ");
+		int a[][] = new int[3][3];
+		
 		for(int i=0;i<a.length;i++)
 		{
 			for(int j=0;j<a[i].length;j++)
@@ -14,8 +27,30 @@ public class LeftRightDiagonalSum
 				a[i][j] = xyz.nextInt();
 			}
 		}
-		System.out.println("\n Display Two Diamentional Array \n");
-		
+		System.out.println("=================");
+		System.out.println("DISPLAY MATRIX ");
+		System.out.println("=================");
+
+		int lsum = 0;
+		int rsum = 0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a[i].length;j++)
+			{
+				System.out.print("  "+a[i][j]);
+				
+				if(i==j)
+				{
+					lsum += a[i][j];
+				}
+				if(j==2-i)
+				{
+					rsum += a[i][j];
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("LEFT DIAGONAL SUM : "+lsum);
+		System.out.println("RIGHT DIAGONAL SUM : "+rsum);
 	}
 }
-
